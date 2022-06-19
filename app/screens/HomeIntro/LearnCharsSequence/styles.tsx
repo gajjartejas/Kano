eb25fb3e3ab606aae85b10df0c0c7c46122c072f
Gajjar Tejas <gajjartejas26@gmail.com>
2022-06-19 23:18:57 +0530

    feat: char screen card swipe logic

--- app/screens/HomeIntro/LearnCharsSequence/styles.tsx
+++ app/screens/HomeIntro/LearnCharsSequence/styles.tsx
@@ -76,6 +76,56 @@ const styles = StyleSheet.create({
     fontWeight: '400',
     fontSize: 20,
   },
+  draggable: {
+    height: 100,
+    marginVertical: 8,
+    marginHorizontal: 8,
+    borderRadius: 8,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  receiver: {
+    height: 100,
+    marginVertical: 8,
+    marginHorizontal: 8,
+    borderRadius: 8,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dragging: {
+    opacity: 0.1,
+  },
+  practiceCardsContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  practiceCardContainer: {
+    flex: 1,
+  },
+  practiceCardText: {
+    fontFamily: Config.Fonts.NotoSansGujarati.Regular,
+    fontWeight: '400',
+    fontSize: 40,
+  },
+  receiving: {
+    borderWidth: 2,
+  },
+  nextButtonStyle: {
+    marginBottom: 16,
+    marginHorizontal: 20,
+    paddingVertical: 6,
+  },
 });
 
 export default styles;
