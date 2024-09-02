ac76e4410ccbc6900b0777897537f5e931b9b3cc
Tejas G <gajjartejas26@gmail.com>
2024-09-02 23:11:46 +0530

    feat: update react native version to 0.74.3 (#93)

--- ios/Kano/AppDelegate.mm
+++ ios/Kano/AppDelegate.mm
@@ -1,5 +1,4 @@
 #import "AppDelegate.h"
-#import "RNBootSplash.h" // ⬅️ add the header import
 #import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
@@ -8,39 +7,28 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  // Add me --- \/
   [FIRApp configure];
-  // Add me --- /\
-  
+
   self.moduleName = @"Kano";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
-}
-
-// ⬇️ Add this before file @end
-- (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
-                          moduleName:(NSString *)moduleName
-                           initProps:(NSDictionary *)initProps {
-  UIView *rootView = [super createRootViewWithBridge:bridge
-                                          moduleName:moduleName
-                                           initProps:initProps];
-
-  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView]; // ⬅️ initialize the splash screen
-
-  return rootView;
 }
 
 @end
